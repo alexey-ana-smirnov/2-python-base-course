71af991210d9f63778dcf09ae2e7521e1444d864
alexey-ana-smirnov <alexey.ana.smirnov@megafon.ru>
2021-01-07 13:51:31 +0400

    удаление реешний

--- lesson-2/hw2_2.sol
+++
@@ -1,5 +0,0 @@
-C:\Users\alex\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/alex/PycharmProjects/pythonProject/hw2_2.py
-Введите элементы массива через пробел: 1 2 3 4 5 6 7
-[2, 1, 4, 3, 6, 5, 7]
-
-Process finished with exit code 0
--- lesson-2/hw2_5.sol
+++
@@ -1,5 +0,0 @@
-C:\Users\alex\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/alex/PycharmProjects/pythonProject/hw2_5.py
-Введите новый элемент рейтинга: 0
-[8, 7, 7, 5, 4, 3, 3, 1, 0]
-
-Process finished with exit code 0
