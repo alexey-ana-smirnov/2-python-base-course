cb99ca1c2975eba82ae2433981b7b3b3003f6df5
alexey-ana-smirnov <alexey.ana.smirnov@megafon.ru>
2021-01-07 12:42:46 +0400

    Устранение замечаний

--- lesson-1/hw2.sol
+++
@@ -1,5 +0,0 @@
-C:\Users\alex\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/alex/PycharmProjects/pythonProject/hw2.py
-Введите время в секундах: 1234567
-Время в формате hh:mm:ss = 342:56:07
-
-Process finished with exit code 0
